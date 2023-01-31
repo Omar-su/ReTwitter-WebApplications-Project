893d9ac25bc64a0579cb5764c5ae9b65d3315b70
felixedholm <felix.edholm@gmail.com>
2023-01-31 14:29:42 +0100

    Added unit test for liking tweets

--- server/src/service/tweet.test.ts
+++ server/src/service/tweet.test.ts
@@ -8,3 +8,17 @@ test("If a tweet is added to the list then it should be in the list", async () =
   const tweets = await tweetService.getTweets();
   expect(tweets.some((tweet) => tweet.description === desc)).toBeTruthy();
 })
+
+test("If a tweet with 5 likes is liked, the number of likes is 6",async () => {
+  const desc = "Like this tweet";
+  const author = "test name";
+  const tweetService = makeTweetService();
+  await tweetService.tweet(author, desc);
+  const tweets = await tweetService.getTweets();
+  const testTweet = tweets.pop();
+  if(testTweet){
+    testTweet.numberOfLikes = 5;
+  tweetService.likeTweet(testTweet.id);
+  expect(testTweet.numberOfLikes = 6)
+  }
+})
