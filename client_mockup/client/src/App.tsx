c51abd5687680ad554b157881580e59478838ce3
bobby <gp.bobbelty@gmail.com>
2023-02-13 11:25:03 +0100

    Loginpage
    also refactored app to home and setup react router dom (login and home)

--- client_mockup/client/src/App.tsx
+++ client_mockup/client/src/App.tsx
@@ -1,67 +1,23 @@
-import React, {Children, useEffect, useState} from 'react';
 import './App.css';
-import axios from 'axios';
-
-interface Tweet {
-  id : number;
-  author : string;
-  description : string
-  numberOfLikes : number;
-  numberOfReplies : number;
-}
+import Login from './Components/Login/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Components/Home/HomePage";
 
 function App() {
-  const[tweets, setTweets] = useState<Tweet[]>([]);
-
-  async function updateTweets(){
-    const response = await axios.get<Tweet[]>("http://localhost:9090/tweet");
-    setTweets(response.data);
-  }
-
-  useEffect(()=>{
-    updateTweets();
-  }, [tweets]);
-
-
-
   return (
-    <div>
-      <h1>Tweet Feed</h1>
-      <div>
-        {tweets.map((tweet) => <TweetItem key={tweet.id} author = {tweet.author} description = {tweet.description} 
-        numberOfLikes={ async () => {
-          await axios.post(`http://localhost:9090/tweet/${tweet.id}`);
-          updateTweets();
-        }} numberOfReplies={tweet.numberOfReplies}>{tweet.numberOfLikes}</TweetItem>)}
-      </div>
-    </div>
+    <Routes>
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
-interface TweetItemProps{
-  key : number;
-  author : string;
-  description : string
-  numberOfLikes : () => Promise<void>;
-  numberOfReplies : number;
-  children?: React.ReactNode
-}
+const AppWrapper = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
 
-function TweetItem({key, author, description, numberOfLikes, numberOfReplies, children} : TweetItemProps){
-  return <div>
-    <img src="" alt="" />
-    <div className='tweet-info'>
-      <p className='id'>{key}</p>
-      <p className='author'>{author}</p>
-      <p className='tweet-description'>{description}</p>
-      <div>
-        <button onClick={numberOfLikes}>
-          {children}
-        </button>
-        <span>{numberOfReplies}</span>
-      </div>
-    </div>
-  </div>
-}
-
-export default App;
+export default AppWrapper;
