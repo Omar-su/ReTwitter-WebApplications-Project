a28f3234267ed9eff3e038ca2d9d62457779f92d
felixedholm <felix.edholm@gmail.com>
2023-02-16 10:54:28 +0100

    Minor fixes

--- client_mockup/client/src/Profile.tsx
+++ client_mockup/client/src/Profile.tsx
@@ -1,4 +1,4 @@
-import React, {Children, useEffect, useState} from 'react';
+import React, { Children, useEffect, useState } from 'react';
 import './App.css';
 import './Profile.css'
 import axios from 'axios';
@@ -7,46 +7,60 @@ import { TweetItem } from './App';
 import { Reply, Tweet } from './App';
 
 interface User {
-  userNameID : string;
-  ownerName : string;
-  bio : string;
-  followers : number;
-  following : number;
-  Tweets : Tweet[];
+  userNameID: string;
+  ownerName: string;
+  bio: string;
+  followers: number;
+  following: number;
+  Tweets: Tweet[];
 }
 
 
 function Profile() {
-const[profileInfo, setProfileInfo] = useState<User>();
+  const [profileInfo, setProfileInfo] = useState<User>();
 
-async function updateProfileInfo(){
+  async function updateProfileInfo() {
     const response = await axios({
       method: 'get',
       url: "http://localhost:9090/profile/account1",
     });
     setProfileInfo(response.data);
-}
+  }
 
-useEffect(() =>{
+  useEffect(() => {
     updateProfileInfo();
-}, []);
+  }, []);
 
-    return <div> 
-        <div className="profile-information">
-          <h1>Account </h1>
-          {!profileInfo ?  "" : <ProfileInfo key={profileInfo.userNameID}
-          userName={profileInfo.userNameID} ownerName = {profileInfo.ownerName} 
-          bio = {profileInfo.bio} followers ={1} following = {2} ></ProfileInfo> }
-        </div>
-        <div className="profile-feed">
-        {!profileInfo ?  "" : profileInfo.Tweets.slice(0).reverse().map((tweet) => <TweetItem key={tweet.id}
-          id={tweet.id} replies={tweet.replies} author = {tweet.author} 
-          description = {tweet.description} numberOfLikes={ async () => {
-            await axios.post(`http://localhost:9090/tweet/${tweet.id}`);
-          }} numberOfReplies={tweet.numberOfReplies}>{tweet.numberOfLikes}</TweetItem> 
-        )}
-        </div>
+  return <div className="profile">
+    <div id="profile-information">
+      <h1>Account </h1>
+      {!profileInfo ? "" :
+        <ProfileInfo
+          key={profileInfo.userNameID}
+          userName={profileInfo.userNameID}
+          ownerName={profileInfo.ownerName}
+          bio={profileInfo.bio}
+          followers={1}
+          following={2} 
+        ></ProfileInfo>}
     </div>
+    <div id="profile-feed">
+      {/*Reverse list of tweets so they are in chronological order*/}
+      {!profileInfo ? "" : profileInfo.Tweets.slice(0).reverse().map((tweet) =>
+        <TweetItem
+          key={tweet.id}
+          id={tweet.id}
+          replies={tweet.replies}
+          author={tweet.author}
+          description={tweet.description}
+          numberOfLikes={async () => {
+            await axios.post(`http://localhost:9090/tweet/${tweet.id}`);
+          }}
+          numberOfReplies={tweet.numberOfReplies}>{tweet.numberOfLikes}
+        </TweetItem>
+      )}
+    </div>
+  </div>
 }
 
 export default Profile;
