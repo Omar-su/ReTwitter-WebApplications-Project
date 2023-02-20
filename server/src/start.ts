deccabf3dbb2088d1004ae67d4d6dbeed928a56c
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-02-20 11:23:18 +0100

    Session set up

--- server/src/start.ts
+++ server/src/start.ts
@@ -5,6 +5,19 @@ import cors from "cors";
 
 export const app = express();
 
+import session from "express-session";
+
+app.use(session({
+  secret : "Your secret key", // TODO Move to separate file. DO NOT UPLOAD TO GITHUB!!!!
+  resave : false,
+  saveUninitialized : true
+}));
+
+app.use(cors({
+  origin: true,
+  credentials : true
+}));
+
 app.use(express.json());
 app.use(cors());
 app.use("/", tweetRouter);
