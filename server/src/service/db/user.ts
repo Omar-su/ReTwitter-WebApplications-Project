3c5a6f3fbdb807df8d920180bfb45c8a975e3918
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-02-26 13:37:38 +0100

    Can create users in database

---
+++ server/src/service/db/user.ts
@@ -0,0 +1,34 @@
+import { userModel } from "../../db/user";
+import { User } from "../../model/profile";
+
+export async function createUser(username : string, name : string, email : string, password : string) : Promise<boolean> {
+  if (await userModel.findOne({username : username, email : email})){
+    return false;
+  }
+
+  await userModel.create({
+    username : username,
+    name : name,
+    email : email,
+    bio : "",
+    followers : 0,
+    following : 0,
+    password : password,
+    tweets : []
+  });
+
+  return true;
+} 
+
+
+
+export async function findUser(username : string, password : string) : Promise<User | undefined> {
+  const user = await userModel.findOne({username : username, password : password});
+
+  if (user == null) {
+      return undefined;
+  }
+  
+  return user;
+}
+
