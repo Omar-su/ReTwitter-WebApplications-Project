39a6863b986213df20663e493d4260d526fbe9c2
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-08 18:13:10 +0100

    Logout request connected with frontend

--- client_mockup/client/src/components/home/NavBar.tsx
+++ client_mockup/client/src/components/home/NavBar.tsx
@@ -1,5 +1,27 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+axios.defaults.withCredentials = true
 
 export function NavBar() {
+
+  const navigate = useNavigate();
+
+  const navigatePage = (link: string) => {
+    navigate(link);
+  }
+
+  async function LogOut(){
+    await axios.post("http://localhost:9090/user/logout")
+      .then(function (response) {
+        console.log(response);
+        navigatePage("/login");
+      })
+      .catch(function (error) {
+        console.log(error);
+      }); 
+  }
+
   return (
     <div className="col-2">
       Menu
@@ -37,6 +59,11 @@ export function NavBar() {
         <li className="nav-item">
           <a className="nav-link top-buffer menu-links nav-button" href="#">
             Settings
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link top-buffer menu-links nav-button" href="#" onClick={LogOut}>
+            Log out
           </a>
         </li>
       </ul>
