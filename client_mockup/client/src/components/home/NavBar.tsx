622792c3f7cc8d92e98e3cce15be513ddc17b3fa
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-08 18:25:21 +0100

    Navigation to profile is implemented

--- client_mockup/client/src/components/home/NavBar.tsx
+++ client_mockup/client/src/components/home/NavBar.tsx
@@ -1,9 +1,26 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { User } from "../../Interfaces";
 
 axios.defaults.withCredentials = true
 
 export function NavBar() {
+
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const response = await axios.get<User>('http://localhost:9090/user/current_user');
+        setCurrentUser(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchUser();
+  }, []);
 
   const navigate = useNavigate();
 
@@ -27,42 +44,42 @@ export function NavBar() {
       Menu
       <ul className="nav flex-column">
         <li className="nav-item">
-          <a className="nav-link active top-buffer menu-links nav-button" aria-current="page" href="/home">
+          <a className="nav-link active top-buffer menu-links nav-button" aria-current="page" href="" onClick={() => navigatePage("/home")}>
             Home
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link top-buffer menu-links nav-button" href="notifications.html">
+          <a className="nav-link top-buffer menu-links nav-button" href="">
             Notifications
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link top-buffer menu-links nav-button" href="messages.html">
+          <a className="nav-link top-buffer menu-links nav-button" href="">
             Messages
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link top-buffer menu-links nav-button" href="bookmarks.html">
+          <a className="nav-link top-buffer menu-links nav-button" href="">
             Bookmark
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link top-buffer menu-links nav-button" href="profile.html">
+          <a className="nav-link top-buffer menu-links nav-button" href="" onClick={() => navigatePage(`/profile/${currentUser?.userNameID}`)}>
             Profile
           </a>
         </li>
         <li className="nav-item">
-          <a id="postbutton" className="nav-link top-buffer menu-links nav-button" href="#">
+          <a id="postbutton" className="nav-link top-buffer menu-links nav-button" href="">
             Tweet
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link top-buffer menu-links nav-button" href="#">
+          <a className="nav-link top-buffer menu-links nav-button" href="">
             Settings
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link top-buffer menu-links nav-button" href="#" onClick={LogOut}>
+          <a className="nav-link top-buffer menu-links nav-button" href="" onClick={LogOut}>
             Log out
           </a>
         </li>
