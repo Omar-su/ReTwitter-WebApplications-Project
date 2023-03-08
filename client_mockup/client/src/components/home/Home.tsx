9379e0d8853834e2ba637d26f712df09ff95fd07
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-08 20:05:02 +0100

    Home page css

--- client_mockup/client/src/components/home/Home.tsx
+++ client_mockup/client/src/components/home/Home.tsx
@@ -5,24 +5,20 @@ import TweetPage from './TweetItem';
 import { TweetFeed } from './TweetFeed';
 import { NavBar } from './NavBar';
 import { NewsItems } from './NewsItem';
-// import { useNavigate } from 'react-router-dom';
-
-// const navigate = useNavigate();
-
-// const navigatePage = (link: string) => {
-//   navigate(link);
-// }
+import './Home.css'
 
 function Home() {
   return (
-    <div className="container text-center">
-      <div className="row align-items-start">
-        <NavBar></NavBar>
-        <div className="col">
-          <TweetFeed></TweetFeed>
-        </div>
-        <div className="col">
-          <NewsItems></NewsItems>
+    <div className='home_body'>
+      <div className="container text-center">
+        <div className="row align-items-start">
+          <NavBar></NavBar>
+          <div className="col">
+            <TweetFeed></TweetFeed>
+          </div>
+          <div className="col">
+            <NewsItems></NewsItems>
+          </div>
         </div>
       </div>
     </div>
