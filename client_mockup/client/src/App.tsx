254d768870a3b2bd8d0637b845302f17fa3ed0db
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-08 18:35:06 +0100

    Connected to react dom

--- client_mockup/client/src/App.tsx
+++ client_mockup/client/src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BookmarksPage } from "./components/bookmarks/BookmarkPage";
 import Home from "./components/home/Home";
 import HomePage from "./components/home/TweetItem";
 import CreateAccount from "./components/login/CreateAccount";
 import Login from "./components/login/Login";
 import ResetPassword from "./components/login/ResetPassword";
+import { Messages } from "./components/message_system/Messages";
+import { NotificationPage } from "./components/notifications/NotificationPage";
 import ProfilePage from "./components/profile/ProfilePage";
+import { SettingsPage } from "./components/settings/Settings";
 
 
 function App() {
@@ -12,6 +16,10 @@ function App() {
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/messages" element={<Messages />} />
+      <Route path="/bookmarks" element={<BookmarksPage />} />
+      <Route path="/settings" element={<SettingsPage />} />
+      <Route path="/notifications" element={<NotificationPage />} />
       <Route path="/reset" element={<ResetPassword />} />
       <Route path="/register" element={<CreateAccount />} />
       <Route path="/profile/:userNameID" element={<ProfilePage />} />
