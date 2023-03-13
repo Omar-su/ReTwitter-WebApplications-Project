cf0c3817ed46e34f7d766b2ee1717b682430e480
felixedholm <felix.edholm@gmail.com>
2023-03-13 10:49:59 +0100

    Add url for testDB

--- server/src/db/conn_url_origin.ts
+++ server/src/db/conn_url_origin.ts
@@ -2,4 +2,4 @@
 
 export const connUrlOrigin = "mongodb+srv://omarsu:twitterclone@twittercluster.o2gwrlp.mongodb.net/test";
 
-export const connUrlTest = "";
+export const connUrlTest = "mongodb+srv://omarsu:twitterclone@twittercluster.o2gwrlp.mongodb.net/db_for_unit_tests";
