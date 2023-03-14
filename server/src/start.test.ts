301f10162052f69232710c384d605d090ff6d4a3
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-14 12:41:03 +0100

    Tests for tweet endpoints

--- server/src/start.test.ts
+++ server/src/start.test.ts
@@ -94,7 +94,7 @@ test("User tweeting error handling", async () => {
     const descriptionBadType = 123;
     await sess
     .post('/tweet')
-    .send({descriptionBadType : descriptionBadType}).expect(400);
+    .send({description : descriptionBadType}).expect(400);
 
     // Log out
     await sess
@@ -110,24 +110,122 @@ test("User tweeting error handling", async () => {
 test("User liking a tweet end-to-end", async () => {
     
     const description = "Test description";
-    const res1 = await sess
+    const res = await sess
     .post('/tweet')
-    .send({description : description}).expect(201);
-    expect(res1.body.description).toEqual(description);
+    .send({description : description});
 
-    // Should recieve a 400 status error if description has other type than string
-    const descriptionBadType = 123;
-    await sess
-    .post('/tweet')
-    .send({descriptionBadType : descriptionBadType}).expect(400);
+    await sess.post(`/tweet/${res.body.id}`)
+    .send().expect(200);
+
+    const negativeIdNr = "-1234";
+
+    await sess.post(`/tweet/${negativeIdNr}`)
+    .send().expect(400);
+
+    // Id of tweet not created 
+    const fakeIdNr = "12345";
+    // Should not find the tweet
+    await sess.post(`/tweet/${fakeIdNr}`)
+    .send().expect(404);
 
     // Log out
     await sess
     .post('/user/logout')
     .send().expect(200);
 
-    await sess
-    .post('/tweet')
-    .send({description : description}).expect(401);
+    await sess.post(`/tweet/${res.body.id}`)
+    .send().expect(401);
+    
 
+});
+
+
+
+test("User getting feed tweets end-to-end", async () => {
+    
+    const tweetDesc1 = "Test description 1";
+    const tweetDesc2 = "Test description 2";
+    const tweetDesc3 = "Test description 3";
+
+    const res = await sess
+    .post('/tweet')
+    .send({description : tweetDesc1}).expect(201);
+
+    const res2 = await sess
+    .post('/tweet')
+    .send({description : tweetDesc2}).expect(201);
+
+    const res3 = await sess
+    .post('/tweet')
+    .send({description : tweetDesc3}).expect(201);
+
+    const feedTweets = await sess.get('/tweet/feed')
+    .send().expect(200);
+    
+    const tweetIds = [res.body.id, res2.body.id, res3.body.id];
+
+    const allIdsInFeed = tweetIds.every((id) => feedTweets.body.some((tweet: { id: any; }) => tweet.id === id));
+    
+    expect(allIdsInFeed).toBe(true);
+
+    // Log out
+    await sess
+    .post('/user/logout')
+    .send().expect(200);
+
+    await sess.get(`/tweet/feed`)
+    .send().expect(401);
+    
+
+});
+
+
+
+test("User deleting a tweet end-to-end", async () => {
+    
+    const description = "Test description";
+    const res1 = await sess
+    .post('/tweet')
+    .send({description : description});
+
+    await sess.delete(`/tweet/${res1.body.id}`)
+    .send().expect(200);
+
+    const negativeIdNr = "-1234";
+
+    await sess.post(`/tweet/${negativeIdNr}`)
+    .send().expect(400);
+
+    // Id of tweet not created 
+    const fakeIdNr = "12345";
+    // Should not find the tweet
+    await sess.post(`/tweet/${fakeIdNr}`)
+    .send().expect(404);
+
+    
+    const res2 = await sess
+    .post('/tweet')
+    .send({description : description});
+
+    // Log out
+    await sess
+    .post('/user/logout')
+    .send().expect(200);
+
+    await sess.delete(`/tweet/${res2.body.id}`)
+    .send().expect(401);
+    
+
+});
+
+afterEach(async () => {
+    if (databaseModels.getReplyModel().db.name == "db_for_unit_tests") {
+        await databaseModels.getReplyModel().deleteMany({});
+    }
+    if (databaseModels.getTweetModel().db.name == "db_for_unit_tests") {
+        await databaseModels.getTweetModel().deleteMany();
+    }
+    if (databaseModels.getUserModel().db.name == "db_for_unit_tests") {
+        await databaseModels.getUserModel().deleteMany({});
+    }
 });
