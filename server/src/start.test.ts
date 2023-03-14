f06dbe55e8a3405dadda0e18c94105bf7333b05c
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-14 14:59:29 +0100

    testing reply end points

--- server/src/start.test.ts
+++ server/src/start.test.ts
@@ -1,55 +1,4 @@
-// import * as SuperTest from "supertest";
-// import { TweetInterface } from "./model/interfaces/tweet.interface";
-// import { app } from "./start";
 
-// const request = SuperTest.default(app);
-
-// test("End-to-end test", async () => {
-//     const description = "Test description";
-//     const author = "author test";
-//     const res1 = await request.post("/tweet").send({author: author, description : description});
-//     expect(res1.statusCode).toEqual(201);
-//     expect(res1.body.description).toEqual(description);
-//     const res2 = await request.get("/tweet");
-//     expect(res2.statusCode).toEqual(200);
-//     expect(res2.body.map((tweet : TweetInterface) => tweet.description)).toContain(description);
-// });
-
-// test("Follow test", async () => {
-//     const userID1 = "account1";
-//     const ownerName1 = "owner1";
-//     const bio1 = "bio1"
-
-//     const userID2 = "account2";
-//     const ownerName2 = "owner2";
-//     const bio2 = "bio2"
-
-//     const createRes1 = await request.post("/newuser").send({userID: userID1, ownerName : ownerName1,
-//     bio : bio1});
-//     expect(createRes1.statusCode).toEqual(201);
-
-//     const createRes2 = await request.post("/newuser").send({userID: userID2, ownerName : ownerName2,
-//         bio : bio2});
-//         expect(createRes2.statusCode).toEqual(201);
-
-
-//     const followRes = await request.post("/profile/account1/follow").send({followee: userID1,
-//         follower: userID2});
-//         expect(followRes.statusCode).toEqual(200);
-
-//     const profilesRes = await request.get("/profiles");
-//     expect(profilesRes.statusCode).toEqual(200);
-    
-//     const followedAccount = profilesRes.body.find((profile: { userNameID: string; }) => 
-//     {return profile.userNameID === userID1});
-//     expect(followedAccount.getFollowers === 1);
-
-//     const followingAccount = profilesRes.body.find((profile: { userNameID: string; }) => 
-//     {return profile.userNameID === userID2});
-//     expect(followingAccount.following === 1);
-
-
-// });
 
 import supertest from 'supertest';
 import { getDatabaseModels } from './db/connect_database';
@@ -61,6 +10,8 @@ import { app } from './start';
 const request = supertest(app);
 const sess = session(app);
 let databaseModels = getDatabaseModels(connUrlTest);
+jest.setTimeout(20000); // set timeout to 10 seconds
+
 
 beforeEach(async () => {
     await sess
@@ -82,7 +33,7 @@ test('User logout', async () => {
 
 });
 
-test("User tweeting error handling", async () => {
+test("User tweeting end-to-end tests", async () => {
     
     const description = "Test description";
     const res1 = await sess
@@ -217,6 +168,84 @@ test("User deleting a tweet end-to-end", async () => {
     
 
 });
+
+
+test("User commenting  on a tweet end-to-end", async () => {
+    
+    const description = "Test description";
+    const res1 = await sess
+    .post('/tweet')
+    .send({description : description}).expect(201);
+
+    const descriptionReply = "Test description reply";
+    await sess
+    .post(`/reply/${res1.body.id}`)
+    .send({description : descriptionReply}).expect(201);
+
+    // Should recieve a 400 status error if description has other type than string
+    const descriptionBadType = 123;
+    await sess
+    .post(`/reply/${res1.body.id}`)
+    .send({description : descriptionBadType}).expect(400);
+
+    const negativeIdNr = "-1234";
+
+    await sess.post(`/reply/${negativeIdNr}`)
+    .send({description : descriptionReply}).expect(400);
+
+    // Id of tweet not created 
+    const fakeIdNr = "12345";
+    // Should not find the tweet
+    await sess.post(`/reply/${fakeIdNr}`)
+    .send({description : descriptionReply}).expect(404);
+
+    // Log out
+    await sess
+    .post('/user/logout')
+    .send().expect(200);
+
+    await sess.post(`/reply/${res1.body.id}`)
+    .send({description : descriptionReply}).expect(401);
+
+});
+
+
+test("User getting replies on a tweet end-to-end", async () => {
+
+    const description = "Test description";
+    const res1 = await sess
+    .post('/tweet')
+    .send({description : description}).expect(201);
+
+    const descriptionReply1 = "Test description reply";
+    await sess
+    .post(`/reply/${res1.body.id}`)
+    .send({description : descriptionReply1}).expect(201);
+
+    const descriptionReply2 = "Test description reply";
+    await sess
+    .post(`/reply/${res1.body.id}`)
+    .send({description : descriptionReply2}).expect(201);
+
+    const descriptionReply3 = "Test description reply";
+    await sess
+    .post(`/reply/${res1.body.id}`)
+    .send({description : descriptionReply3}).expect(201);
+
+    const repliesOnTestTweet = await sess
+    .get(`/reply/feed/replies/${res1.body.id}`)
+    .send().expect(200);
+
+    const repliesDescs = [descriptionReply1 , descriptionReply2 , descriptionReply3];
+
+    const allDescsInTweetThread = repliesDescs.every((desc) => repliesOnTestTweet.body.some((reply: any) => reply.description == desc));
+
+    expect(allDescsInTweetThread).toBe(true);
+
+});
+
+
+
 
 afterEach(async () => {
     if (databaseModels.getReplyModel().db.name == "db_for_unit_tests") {
